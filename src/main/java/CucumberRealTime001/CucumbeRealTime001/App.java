099a2d8e9ef7e2500ed83d23f76fafe2ace90dcb
clintonraj1924 <clintonraj07@outlook.com>
2024-11-11 18:33:26 +0530

    1st

---
+++ src/main/java/CucumberRealTime001/CucumbeRealTime001/App.java
@@ -0,0 +1,10 @@
+package CucumberRealTime001.CucumbeRealTime001;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
